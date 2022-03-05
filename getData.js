27a95ad080ca9860b203075d974230b0bdcfecf5
coden-arfat <arfatkhanbd24@gmail.com>
2022-03-05 10:48:48 -0800

    v8
    Signed-off-by: coden-arfat <arfatkhanbd24@gmail.com>

--- getData.js
+++ getData.js
@@ -1,2 +1,13 @@
-const mainData =require('./Data.js')
-console.log(mainData)
+/* note this method is useful when you working node , you can't see output in browser console 
+
+
+const data = require('./Data.json');
+console.log(data.arfat);
+
+*/
+
+/* 
+
+let display data in browser console 
+
+*/
