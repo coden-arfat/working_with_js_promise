6c56cc44118d0f17d162e6506972424d05aae02a
coden-arfat <arfatkhanbd24@gmail.com>
2022-03-05 09:14:25 -0800

    v2
    Signed-off-by: coden-arfat <arfatkhanbd24@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('hello world')
+
+const promise = new Promise((resolve,reject)=>{
+
+  let isGood = true
+  if(isGood=true){
+    resolve('success promise')
+  }else{
+   reject('unsuccessful promise')
+  }
+})
