def598a329caee5d518f81f15139019c74828138
coden-arfat <arfatkhanbd24@gmail.com>
2022-03-07 09:33:59 -0800

    v14
    Signed-off-by: coden-arfat <arfatkhanbd24@gmail.com>

--- fetch_api/fetch_api.js
+++ fetch_api/fetch_api.js
@@ -1,5 +1,5 @@
 console.log('fetch api pratics')
-const URL = 'https://jsonplaceholder.typicode.com/todos'
+const URL = 'https://jsonplaceholder.typicode.com/todos/1'
 
 fetch(URL).then(response=> response.json()).then(data=> console.log(data))
 
