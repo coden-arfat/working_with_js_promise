265fd55a233ae46ced2fed93c9f134a40a4915d8
coden-arfat <arfatkhanbd24@gmail.com>
2022-03-07 10:01:32 -0800

    v14
    Signed-off-by: coden-arfat <arfatkhanbd24@gmail.com>

--- fetch_api/fetch_api.js
+++ fetch_api/fetch_api.js
@@ -1,5 +1,16 @@
 console.log('fetch api pratics')
-const URL = 'https://jsonplaceholder.typicode.com/todos/1'
 
-fetch(URL).then(response=> response.json()).then(data=> console.log(data))
+async function getData(){
+const URL = 'https://jsonplaceholder.typicode.com/todos/'
+  fetch(URL).then(response=> response.json()).then(data=> await setTask(data))
+}
+ function setTask(data){
+  console.log(data)
+  for (item of data ){
+    setTimeout((item)=>{
+      document.querySelector('#task').innerText = item.title + item.id
+    },2000)
+  }
+}
 
+getData()
